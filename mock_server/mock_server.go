9ddf213ea9594f499811157acd62304e06ac3582
Eugene Pokidov <pokidovea@gmail.com>
2017-11-03 00:54:52 +0300

    Adjust simple config and logic

--- mock_server/mock_server.go
+++ mock_server/mock_server.go
@@ -7,9 +7,15 @@ import (
 	"time"
 )
 
+type ResponseMethod struct {
+	Response    string `json:"response"`
+	ContentType string `json:"content_type"`
+}
+
 type Endpoint struct {
-	Url      string `json:"url"`
-	Response string `json:"response"`
+	Url  string         `json:"url"`
+	GET  ResponseMethod `json:"GET"`
+	POST ResponseMethod `json:"POST"`
 }
 
 type MockServer struct {
@@ -18,14 +24,32 @@ type MockServer struct {
 	Endpoints []Endpoint `json:"endpoints"`
 }
 
+func (responseMethod ResponseMethod) writeResponse(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", responseMethod.ContentType)
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w, responseMethod.Response)
+}
+
+func (endpoint Endpoint) getHandler() func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		if req.Method == "GET" && endpoint.GET.Response != "" {
+			endpoint.GET.writeResponse(w)
+			return
+		}
+		if req.Method == "POST" && endpoint.POST.Response != "" {
+			endpoint.POST.writeResponse(w)
+			return
+		}
+		http.NotFound(w, req)
+	}
+}
+
 func (mockServer MockServer) Serve(done chan bool) {
 	mux := http.NewServeMux()
 
 	for _, endpoint := range mockServer.Endpoints {
-		response := endpoint.Response
-		mux.HandleFunc(endpoint.Url, func(w http.ResponseWriter, req *http.Request) {
-			fmt.Fprintf(w, response)
-		})
+		mux.HandleFunc(endpoint.Url, endpoint.getHandler())
 	}
 
 	s := &http.Server{
