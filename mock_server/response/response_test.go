2364861fcdd0151bd63b6fdfe7019553c355a49d
Eugene Pokidov <pokidovea@gmail.com>
2017-11-09 09:01:02 +0300

    Add tests

--- mock_server/response/response_test.go
+++ mock_server/response/response_test.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"path"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -34,7 +36,7 @@ func TestUnmarshallingAllFields(t *testing.T) {
 	assert.Equal(t, http.StatusCreated, response.StatusCode)
 }
 
-func TestWriteResponse(t *testing.T) {
+func TestWriteSimpleResponse(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/simple_url", nil)
 	var response = Response{"{\"a\":1}", "application/json", http.StatusCreated, false}
@@ -47,4 +49,26 @@ func TestWriteResponse(t *testing.T) {
 	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
 	assert.Equal(t, http.StatusCreated, resp.StatusCode)
 	assert.Equal(t, "{\"a\":1}", string(body))
+}
+
+func TestWriteFileResponse(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/simple_url", nil)
+
+	filepath := path.Join(path.Dir(filename), "..", "..", "config", "fixtures", "mimicro.png")
+	var response = Response{filepath, "", http.StatusOK, true}
+
+	response.WriteResponse(w, r)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	assert.Equal(t, "image/png", resp.Header.Get("Content-Type"))
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	fileContent, err := ioutil.ReadFile(filepath)
+	assert.Nil(t, err)
+	assert.Equal(t, fileContent, body)
 }
