0face5b18af5a1ff22ef1b98a6900eab73061be9
Eugene Pokidov <pokidovea@gmail.com>
2017-11-11 12:51:28 +0300

    Some tests on WriteResponse

--- mock_server/response/response_test.go
+++ mock_server/response/response_test.go
@@ -1,66 +1,49 @@
 package response
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"path"
 	"runtime"
 	"testing"
+	"text/template"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUnmarshallingOnlyBody(t *testing.T) {
-	str := `{"body":"OK"}`
+func TestWriteTemplateResponse(t *testing.T) {
+	tmpl := template.New("template")
+	tmpl.Parse(`{"passed_value": "{{.var}}"}`)
 
-	var response Response
-	err := json.Unmarshal([]byte(str), &response)
-	assert.Nil(t, err)
+	response := Response{tmpl, "", "application/json", http.StatusCreated}
+	router := mux.NewRouter()
+	router.HandleFunc("/simple_url/{var}", response.WriteResponse)
 
-	assert.Equal(t, "OK", response.Body)
-	assert.Equal(t, "text/plain", response.ContentType)
-	assert.Equal(t, http.StatusOK, response.StatusCode)
-}
-
-func TestUnmarshallingAllFields(t *testing.T) {
-	str := `{"body":"{}", "content_type":"application/json", "status_code": 201}`
-
-	var response Response
-	err := json.Unmarshal([]byte(str), &response)
-	assert.Nil(t, err)
-
-	assert.Equal(t, "{}", response.Body)
-	assert.Equal(t, "application/json", response.ContentType)
-	assert.Equal(t, http.StatusCreated, response.StatusCode)
-}
-
-func TestWriteSimpleResponse(t *testing.T) {
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/simple_url", nil)
-	var response = Response{"{\"a\":1}", "application/json", http.StatusCreated, false}
+	req := httptest.NewRequest("POST", "/simple_url/1", nil)
 
-	response.WriteResponse(w, r)
+	router.ServeHTTP(w, req)
 
 	resp := w.Result()
 	body, _ := ioutil.ReadAll(resp.Body)
 
 	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
 	assert.Equal(t, http.StatusCreated, resp.StatusCode)
-	assert.Equal(t, "{\"a\":1}", string(body))
+	assert.Equal(t, `{"passed_value": "1"}`, string(body))
 }
 
 func TestWriteFileResponse(t *testing.T) {
 	_, filename, _, _ := runtime.Caller(0)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/simple_url", nil)
+	req := httptest.NewRequest("GET", "/simple_url", nil)
 
-	filepath := path.Join(path.Dir(filename), "..", "..", "config", "fixtures", "mimicro.png")
-	var response = Response{filepath, "", http.StatusOK, true}
+	filepath := path.Join(path.Dir(filename), "..", "..", "examples", "mimicro.png")
+	var response = Response{nil, filepath, "", http.StatusOK}
 
-	response.WriteResponse(w, r)
+	response.WriteResponse(w, req)
 
 	resp := w.Result()
 	body, _ := ioutil.ReadAll(resp.Body)
@@ -72,3 +55,42 @@ func TestWriteFileResponse(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, fileContent, body)
 }
+
+// func TestUnmarshallingOnlyBody(t *testing.T) {
+// 	str := `{"body":"OK"}`
+
+// 	var response Response
+// 	err := json.Unmarshal([]byte(str), &response)
+// 	assert.Nil(t, err)
+
+// 	assert.Equal(t, "OK", response.Body)
+// 	assert.Equal(t, "text/plain", response.ContentType)
+// 	assert.Equal(t, http.StatusOK, response.StatusCode)
+// }
+
+// func TestUnmarshallingAllFields(t *testing.T) {
+// 	str := `{"body":"{}", "content_type":"application/json", "status_code": 201}`
+
+// 	var response Response
+// 	err := json.Unmarshal([]byte(str), &response)
+// 	assert.Nil(t, err)
+
+// 	assert.Equal(t, "{}", response.Body)
+// 	assert.Equal(t, "application/json", response.ContentType)
+// 	assert.Equal(t, http.StatusCreated, response.StatusCode)
+// }
+
+// func TestWriteSimpleResponse(t *testing.T) {
+// 	w := httptest.NewRecorder()
+// 	r := httptest.NewRequest("POST", "/simple_url", nil)
+// 	var response = Response{"{\"a\":1}", "application/json", http.StatusCreated, false}
+
+// 	response.WriteResponse(w, r)
+
+// 	resp := w.Result()
+// 	body, _ := ioutil.ReadAll(resp.Body)
+
+// 	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+// 	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+// 	assert.Equal(t, "{\"a\":1}", string(body))
+// }
