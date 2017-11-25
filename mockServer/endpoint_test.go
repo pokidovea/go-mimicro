758bfc931950d92186a1320749cf8761535149bf
Eugene Pokidov <pokidovea@gmail.com>
2017-11-25 11:18:23 +0300

    expand endpoint tests

--- mockServer/endpoint_test.go
+++ mockServer/endpoint_test.go
@@ -22,80 +22,81 @@ func (msg *responseLogMessage) writeResponseLog(serverName, URL, method string, 
 	msg.StatusCode = statusCode
 }
 
-func createEndpoint() Endpoint {
+func TestHandleResponse(t *testing.T) {
 	str := `
 url: /simple_url
 GET:
-    template: "{}"
+    template: GET
     headers:
         content-type: application/json
+    status_code: 201
 POST:
-    template: OK
+    template: POST
+    headers:
+        content-type: application/json
+    status_code: 201
+PUT:
+    template: PUT
+    headers:
+        content-type: application/json
+    status_code: 201
+PATCH:
+    template: PATCH
+    headers:
+        content-type: application/json
+    status_code: 201
+DELETE:
+    template: DELETE
+    headers:
+        content-type: application/json
     status_code: 201
 `
 
 	var endpoint Endpoint
-	err := yaml.Unmarshal([]byte(str), &endpoint)
+	yaml.Unmarshal([]byte(str), &endpoint)
 
-	if err != nil {
-		panic(err)
+	logMessage := new(responseLogMessage)
+
+	methods := [...]string{"GET", "POST", "PATCH", "PUT", "DELETE"}
+	handler := endpoint.GetHandler(logMessage.writeResponseLog, "server_name")
+
+	for _, method := range methods {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(method, "/simple_url", nil)
+
+		handler(w, r)
+
+		resp := w.Result()
+
+		body, _ := ioutil.ReadAll(resp.Body)
+		assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+		assert.Equal(t, http.StatusCreated, resp.StatusCode)
+		assert.Equal(t, method, string(body))
+
+		assert.Equal(t, "server_name", logMessage.ServerName)
+		assert.Equal(t, "/simple_url", logMessage.URL)
+		assert.Equal(t, method, logMessage.Method)
+		assert.Equal(t, http.StatusCreated, logMessage.StatusCode)
 	}
-
-	return endpoint
-}
-
-func TestHandleGETResponse(t *testing.T) {
-	endpoint := createEndpoint()
-	logMessage := new(responseLogMessage)
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/simple_url", nil)
-
-	handler := endpoint.GetHandler(logMessage.writeResponseLog, "server_name")
-	handler(w, r)
-
-	resp := w.Result()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-	assert.Equal(t, "{}", string(body))
-
-	assert.Equal(t, "server_name", logMessage.ServerName)
-	assert.Equal(t, "/simple_url", logMessage.URL)
-	assert.Equal(t, "GET", logMessage.Method)
-	assert.Equal(t, 200, logMessage.StatusCode)
-}
-
-func TestHandlePOSTResponse(t *testing.T) {
-	endpoint := createEndpoint()
-	logMessage := new(responseLogMessage)
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/simple_url", nil)
-
-	handler := endpoint.GetHandler(logMessage.writeResponseLog, "server_name")
-	handler(w, r)
-
-	resp := w.Result()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-	assert.Equal(t, http.StatusCreated, resp.StatusCode)
-	assert.Equal(t, "OK", string(body))
-
-	assert.Equal(t, "server_name", logMessage.ServerName)
-	assert.Equal(t, "/simple_url", logMessage.URL)
-	assert.Equal(t, "POST", logMessage.Method)
-	assert.Equal(t, 201, logMessage.StatusCode)
 }
 
 func TestHandleNonexistingResponses(t *testing.T) {
-	endpoint := createEndpoint()
+	str := `
+url: /simple_url
+GET:
+    template: GET
+    headers:
+        content-type: application/json
+    status_code: 201
+`
+
+	var endpoint Endpoint
+	yaml.Unmarshal([]byte(str), &endpoint)
+
 	logMessage := new(responseLogMessage)
 
+	methods := [...]string{"POST", "PATCH", "PUT", "DELETE"}
 	handler := endpoint.GetHandler(logMessage.writeResponseLog, "server_name")
-
-	methods := [...]string{"PATCH", "PUT", "DELETE"}
 
 	for _, method := range methods {
 		w := httptest.NewRecorder()
